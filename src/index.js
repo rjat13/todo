1216dab8acbc14719977b296d9640cd638f18a13
macmini26 <macmini26@macmini26s-Mac-mini.local>
2023-03-10 11:43:31 +0530

    import/export component, create starting point etc.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-// import './index.css';
-import App from './App';
-// import reportWebVitals from './reportWebVitals';
+// 1) import React and ReactDOM library.
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import Demo, {MyFun} from "./Demo";
+import 'bulma/css/bulma.min.css'
+import Card from "./component/Card";
+
+// 2) Get refference to the div with root id.
+const rootEle = document.getElementById('root');
+
+// 3) Tell react to take controll of that element.
+const root = ReactDOM.createRoot(rootEle)
+
+// 4) create component
+const App = () => {
+    let msg = "By there!";
+    if(Math.round(Math.random()*10) > 5){
+        // console.log('if load');
+        msg = "Hello there!";
+    }
+    return (<div className="container">
+                <section className="section">
+                    <h3>{msg}</h3>
+                    <input type='text' placeholder='Hello' />
+                    <hr />
+                    <Demo />
+                    <MyFun />
+                    <Card />
+                </section>
+            </div>)
+}
+
+// 5) show the component on the screen.
+
+root.render(<App />)
